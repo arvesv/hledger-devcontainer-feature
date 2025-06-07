7294d5b51781ccacaeec5ccf2578703aa0b20042
Arve Svendsen <arve.svendsen@outlook.com>
2025-06-07 04:32:34 +0200

    Refactor hledger installation script to ensure proper binary placement and cleanup

--- src/hledger/install.sh
+++ src/hledger/install.sh
@@ -4,4 +4,9 @@ apt install -y curl
 
 curl -o eget.sh https://zyedidia.github.io/eget.sh
 bash eget.sh 
-./eget simonmichael/hledger /usr/local/bin 
+./eget simonmichael/hledger 
+mv hledger /usr/local/bin/hledger
+chmod +x /usr/local/bin/hledger
+hledger --version
+rm eget.sh
+rm eget
